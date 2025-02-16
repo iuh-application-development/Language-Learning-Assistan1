85ac8393eae98f80eaa19f301d57034e5431d3be
VienVien_1026 <vi12091994@gmail.com>
2025-02-16 22:23:46 +0700

    f1: refactor front end

---
+++ dolphin_english/dolphin/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path("gramma/", views.gramma_view, name='gramma'), 
+    path("listen/", views.listen_view,name='listen'),
+]
---
+++ dolphin_english/dolphin/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    # in ra xin chao
+    return render(request, 'dolphin/base.html')
+
+def gramma_view(request):
+    return render(request, 'dolphin/gramma.html')
+
+def listen_view(request):
+    return render(request, 'dolphin/listen.html')
