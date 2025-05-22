9301c453b4932492b27e750036aaf926c91b2b7c
Nguyen Huu Phuc <phucnguyenhuu6504@gmail.com>
2025-05-22 23:01:40 +0700

    Fixxed Login

--- dolphin_english/ptud/settings.py
+++ dolphin_english/ptud/settings.py
@@ -15,9 +15,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -53,7 +49,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google', 
-    'rest_framework',
 ]
 
 
@@ -128,11 +123,6 @@ WSGI_APPLICATION = "ptud.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),  # Tên database (cố định là postgres trên Supabase)
-        'USER': os.getenv('DB_USER'),  # Tên user mặc định trên Supabase
-        'PASSWORD': os.getenv('DB_PASSWORD'),  # Mật khẩu từ connection string
-        'HOST': os.getenv('DB_HOST'),  # Lấy từ Supabase connection string
-        'PORT': os.getenv('DB_PORT'),  # Cổng mặc định cho PostgreSQL
         'NAME': os.getenv('DB_NAME'),  # Tên database (cố định là postgres trên Supabase)
         'USER': os.getenv('DB_USER'),  # Tên user mặc định trên Supabase
         'PASSWORD': os.getenv('DB_PASSWORD'),  # Mật khẩu từ connection string
